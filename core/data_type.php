f5a06ac087e7a2e6f374149ce5f78f2f2f61bdb8
Dario Tecchia <dariotecchia@gmail.com>
2017-08-02 20:06:17 +0200

    UPDATED data_type.php: DATETIME changed in TIMESTAMP

--- core/data_type.php
+++ core/data_type.php
@@ -1,5 +1,5 @@
 <?php
   $MYSQL_DATA_TYPE["String"] = "VARCHAR";
   $MYSQL_DATA_TYPE["Double"] = "DOUBLE";
-  $MYSQL_DATA_TYPE["Datetime"] = "DATETIME";
+  $MYSQL_DATA_TYPE["Datetime"] = "TIMESTAMP";
   $MYSQL_DATA_TYPE["Integer"] = "INT";
